44179948881d58d2efe6485878181f7998a2adba
beginnercoder8 <beginnercoder8@gmail.com>
2024-10-06 10:21:01 +0530

    forgot password api changes

--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { signUp, login,sendOtp, verifyOtp } = require('../controllers/userController');
+const { signUp, login,sendOtp, verifyOtp, forgotPassword } = require('../controllers/userController');
 router.route('/signup').post(signUp);
 router.route('/login').post(login);
 router.route('/otp').post(sendOtp);
 router.route('/verify-otp').post(verifyOtp);
+router.route('/forgot-password').post(forgotPassword);
 module.exports = router;
